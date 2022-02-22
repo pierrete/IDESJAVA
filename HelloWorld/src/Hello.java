5e49c0d017b4a8cf16b5ebf60e623cbb0ce1a862
Pamela Rod <akahime2014@gmail.com>
2022-02-22 02:46:56 -0300

    first commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,27 @@
+public class Hello {
+	public static void main (String[] args) 
+	{
+		
+		int a = 10;
+		int b = 20;
+		
+		System.out.println("Oi, meu chapa!");
+		
+		if(a>b) 
+		{
+			System.out.println("E lá vamos nós!");
+		}
+		
+		else 
+		{
+			System.out.println("Se o pica-pau tivesse informado à polícia, nada disso teria acontecido! ");
+		}
+		
+		if((a==10)&&(b==20)) 
+		{
+			System.out.println("Delicia gelada!\nHummm...... Goxtoso!");
+		}
+		
+	}
+	
+} 
